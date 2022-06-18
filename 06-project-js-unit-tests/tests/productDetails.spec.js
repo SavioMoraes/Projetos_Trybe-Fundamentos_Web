1cfeadf5ad7caeacce406b6b702efcb1992481c4
SavioMoraes <smmsavio@hotmail.com>
2022-06-18 13:44:41 -0300

    Insere Projeto JS Unit Tests

---
+++ 06-project-js-unit-tests/tests/productDetails.spec.js
@@ -0,0 +1,50 @@
+const assert = require('assert');
+const productDetails = require('../src/productDetails');
+
+/*
+  Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
+
+  Parâmetros:
+  - Uma string;
+  - Uma string;
+
+  Comportamento:
+  productDetails('Alcool gel', 'Máscara') // Retorna:
+  [
+    {
+      name: 'Alcool gel'
+      details: {
+        productId: 'Alcool gel123'
+      }
+    },
+    {
+      name: 'Máscara'
+      details: {
+        productId: 'Máscara123'
+      }
+    }
+  ]
+
+  OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
+*/
+
+describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
+  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
+    // ESCREVA SEUS TESTES ABAIXO:
+    // Teste que o retorno da função é um array.
+    assert.strictEqual(typeof productDetails('Alcool gel', 'Máscara'), 'object');
+    // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('Alcool gel', 'Máscara').length, 2);
+    // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof productDetails('Alcool gel', 'Máscara')[0], 'object');
+    assert.strictEqual(typeof productDetails('Alcool gel', 'Máscara')[1], 'object');
+    // Teste que os dois objetos são diferentes entre si.
+    assert.notStrictEqual(productDetails('Alcool gel', 'Máscara')[0], productDetails('Alcool gel', 'Máscara')[1]);
+    // Teste que os dois productIds terminam com 123.
+    const products = productDetails('Alcool gel', 'Máscara');
+    const product1 = products[0].details.productId.substr(products[0].details.productId.length -3);
+    const product2 = products[1].details.productId.substr(products[1].details.productId.length -3);
+    assert.strictEqual(product1, '123');
+    assert.strictEqual(product2, '123');
+  });
+});
